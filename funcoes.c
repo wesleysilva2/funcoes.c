66d30240cd82acc28aa400089e2054af53f06877
wesleysilva2 <ede8021@gmail.com>
2017-03-25 10:48:21 -0300

    funcoes.c

---
+++ funcoes.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h> 
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+//aritimetica questão 2  
+float opera (float a, float b, int c){
+       
+  float z;
+	   
+   if(c==0){
+	z=a+b;
+   }
+	else if(c==1){
+	 z=b-a;
+	}
+		 
+	 else if(c==2){
+	  z=a*b;
+	 }
+		   
+	  else if(c==3){
+	   z=a/b;
+	  }  
+
+return (z);
+      
+  } 
+  
+//comparação questão 1 
+
+int maiordedois(int num1 , int num2){
+  
+   if(num1<num2){
+	  	
+      
+        x=num2;
+       
+    }
+        
+	     else{
+	    	
+      	
+      	   x=num1;
+	    
+		 }
+		
+return (x);
+		
+	
+}
+
+//comparação questão 2
+
+int menordetres(int num1 , int num2, int num3){
+	
+	int z;
+	
+     if(num1<=num2 && num1<=num3){
+     	z=num1;
+     	 
+	 }
+	 
+	   else if(num2<=num1 && num2<=num3){
+	  	  z=num2;
+	  	  
+		    
+	  }
+	     else if(num3<=num1 && num3<=num2){
+	        z=num3;
+	    	  
+		}
+		
+return(z);
+	
+}
+
+//aritimética questão 1 
+
+float soma (int a int b){
+	
+	int c
+	
+	 c=a+b
+      
+	   return (c);
+      
+  } 
+
+		
+	
+	 
+	 
+	
+
+
+
+	
+
+    
+    
+	
+	
+	
